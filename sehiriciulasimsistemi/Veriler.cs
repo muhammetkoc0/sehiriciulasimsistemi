9f2d3bdbfdb36a1e9b8ea6286e768676dc55154e
muhammetkoc0 <121392529+muhammetkoc0@users.noreply.github.com>
2023-11-17 05:07:24 +0300

    veriler adında clas yapısı oluşturularak Admin, Şoför ve yolcu verileri için gerekli kodlar yazıldı...

---
+++ sehiriciulasimsistemi/Veriler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sehiriciulasimsistemi
+{
+    internal class Veriler
+    {
+        string aad;
+        string asoyad;
+        string aid;
+        string asifre;
+        string sad;
+        string ssoyad;
+        string sid;
+        string yad;
+        string ysoyad;
+        string yid;
+        string aplaka;
+        string ytarife;
+        public string Aad { get { return aad; } set { aad = value; } }
+        public string Asoyad { get { return asoyad; } set {  asoyad = value; } }
+        public string Asifre { get { return asifre; } set { asifre = value; } }
+        public string Aid { get { return aid; } set { aid = value; } }
+        public string Sad { get { return sad;  }set { sad = value; } }
+        public string Ssoyad { get { return ssoyad; } set{ ssoyad = value; } }
+        public string Sid { get { return sid; } set { sid = value; } }
+        public string Yad { get { return yad; }set { yad = value; } }
+        public string Ysoyad { get { return ysoyad; } set { ysoyad = value; } }
+        public string Yid { get { return yid; } set { yid = value; } }
+        public string Aplaka {  get { return aplaka; } set {  aplaka = value; } }
+        public string Ytarife {  get { return ytarife; } set { ytarife = value; } }
+    }
+}
